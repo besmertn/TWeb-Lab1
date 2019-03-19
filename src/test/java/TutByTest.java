a971ab435db5b4e821e6fe3959cad6e786310c6a
Aleksandr Bessmertnyi <Aleksandr.Bessmertnyi@parexel.com>
2019-03-19 13:17:40 +0200

    Add authorization test

--- src/test/java/TutByTest.java
+++ src/test/java/TutByTest.java
@@ -1,8 +1,12 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TutByTest {
 
@@ -10,10 +14,13 @@ public class TutByTest {
 
     @Before
     public void setUp() {
-        String pathToChromeDriver = "lib/chromedriver";
+        String pathToChromeDriver = "../../../chromedriver";
         System.setProperty("web.chrome.driver", pathToChromeDriver);
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        driver = new ChromeDriver(options);
     }
 
     @After
@@ -24,5 +31,13 @@ public class TutByTest {
     @Test
     public void TestAuthorization() {
         driver.navigate().to("https://www.tut.by");
+        driver.switchTo().frame(driver.findElement(By.id("top_bar_helper")));
+        driver.findElement(By.cssSelector("#authorize > div > a")).click();
+        driver.findElement(By.cssSelector("form.auth-form input[name='login']")).sendKeys("login");
+        driver.findElement(By.cssSelector("form.auth-form input[name='password']")).sendKeys("password");
+        //driver.findElement(By.cssSelector("form.auth-form input[type='submit']")).click();
+        driver.findElement(By.cssSelector(".b-auth-soc .social__btn--fb")).click();
+        WebElement userName = driver.findElement(By.cssSelector("#authorize .uname"));
+        assert userName.getText().equals("Александр Бессмертный");
     }
 }
